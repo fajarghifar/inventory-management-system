3985a52e6e9ce45f387b65b968c756dfdb2fdeea
MimisK13 <mimisk88@gmail.com>
2023-11-18 19:28:53 +0200

    fix route

--- resources/views/purchases/index.blade.php
+++ resources/views/purchases/index.blade.php
@@ -12,7 +12,7 @@
 
             <div class="col-auto ms-auto d-print-none">
                 <div class="btn-list">
-                    <a href="{{ route('orders.create') }}" class="btn btn-outline-success d-none d-sm-inline-block">
+                    <a href="{{ route('purchases.create') }}" class="btn btn-outline-success d-none d-sm-inline-block">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
                         {{ __('Create') }}
                     </a>

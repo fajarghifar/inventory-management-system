6046d524c3dfa9d3228190ee0c689950b9324cf2
MimisK13 <mimisk88@gmail.com>
2023-11-27 20:27:21 +0200

    PAGINATE: add tabler classes

--- resources/views/vendor/livewire/bootstrap.blade.php
+++ resources/views/vendor/livewire/bootstrap.blade.php
@@ -13,15 +13,21 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
 <div>
     @if ($paginator->hasPages())
         <nav>
-            <ul class="pagination">
+            <ul class="pagination m-0 ms-auto">
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
-                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                        <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                    </li>
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">
+                        <x-icon.chevron-left />
+                        prev
+                    </span>
+                </li>
                 @else
                     <li class="page-item">
-                        <button type="button" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</button>
+                        <button type="button" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" rel="prev" aria-label="@lang('pagination.previous')">
+                            <x-icon.chevron-left />
+                            prev
+                        </button>
                     </li>
                 @endif
 
@@ -29,7 +35,9 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                 @foreach ($elements as $element)
                     {{-- "Three Dots" Separator --}}
                     @if (is_string($element))
-                        <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                        <li class="page-item disabled" aria-disabled="true">
+                            <span class="page-link">{{ $element }}</span>
+                        </li>
                     @endif
 
                     {{-- Array Of Links --}}
@@ -47,12 +55,20 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
                     <li class="page-item">
-                        <button type="button" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</button>
+                        <button type="button"
+                                dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}"
+                                class="page-link" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}"
+                                wire:loading.attr="disabled"
+                                rel="next" aria-label="@lang('pagination.next')">
+                            next
+                            <x-icon.chevron-right />
+                        </button>
                     </li>
                 @else
-                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                        <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                    </li>
+                    <button type="button" class="page-link disabled">
+                        next
+                        <x-icon.chevron-right />
+                    </button>
                 @endif
             </ul>
         </nav>

5df51248388e88ae0b19ccc714aed54c7bf804f1
inerttila <inert.etila@gmail.com>
2025-02-12 12:58:36 +0100

    rm description form the search funtion

--- app/Livewire/SearchProduct.php
+++ app/Livewire/SearchProduct.php
@@ -30,7 +30,6 @@ class SearchProduct extends Component
     {
         $this->search_results = Product::where('name', 'like', '%'.$this->query.'%')
             ->orWhere('code', 'like', '%'.$this->query.'%')
-            ->orWhere('description', 'like', '%'.$this->query.'%')
             ->orWhereHas('category', function ($q) {
                 $q->where('name', 'like', '%'.$this->query.'%');
             })
